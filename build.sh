2265cbed70c118403ac7d660b0a7bea298359348
TheKit <nekit1000@gmail.com>
2022-08-23 23:46:57 +0300

    hack: build.sh: unpack python3-evdev to overlay

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 set -xe
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
+
+# Unpack python3-evdev to overlay until it is added to rootfs
+wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/p/python-evdev/python3-evdev_0.4.7-0ubuntu4_arm64.deb
+dpkg-deb -x python3-evdev_0.4.7-0ubuntu4_arm64.deb "overlay/system"
+
 ./build/build.sh "$@"
